b35c41104181c5bbbcba617da74bfaa33739fd25
Ezekiel <iamwebwiz@gmail.com>
2022-10-26 15:48:32 +0100

    update travis and deployment

--- udagram-api-feed/src/server.ts
+++ udagram-api-feed/src/server.ts
@@ -35,7 +35,7 @@ import {V0_FEED_MODELS} from './controllers/v0/model.index';
   }));
 
   app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", process.env.URL);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
